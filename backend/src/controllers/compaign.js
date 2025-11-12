c37c7a246df86adf949196bed95b8694ffd5d527
Nada.Salha <128420328+NadaSalha@users.noreply.github.com>
2025-11-12 16:00:13 +0200

    render images

--- backend/src/controllers/compaign.js
+++ backend/src/controllers/compaign.js
@@ -1,10 +1,18 @@
 import { getAllCampaigns } from "../services/displayCampaign.js";
+import { getSignedUrlForFile } from "../services/s3-service.js";
 
 
 export const getCampaigns = async (req, res, next) => {
     try {
-        const campaigns = await getAllCampaigns();
-        res.status(200).json(campaigns);
+        const campaigns = await getAllCampaigns();        
+            const campaignsWithUrls = await Promise.all(
+              campaigns.map(async (c) => ({
+                ...c,
+                image: c.image ? await getSignedUrlForFile(c.image) : null,
+              }))
+            );
+        
+        res.status(200).json(campaignsWithUrls);
     } catch (err) {
         console.error("Error fetching campaigns:", err);
         next(err);
