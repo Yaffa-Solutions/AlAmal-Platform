960abb13198b2284d64a91c929e9a44236605ae4
MarwaKHTanani <tananimarwa@gmail.com>
2025-11-20 22:16:00 +0200

    Replace nodemailer with Brevo for sending OTP emails

--- backend/src/services/sendotp.js
+++ backend/src/services/sendotp.js
@@ -1,27 +1,37 @@
-import { transporter } from '../config/mailer.js';
 
 export const sendOTP = (email, code, magicLink) => {
-  return new Promise((resolve, reject) => {
-    transporter.sendMail(
-      {
-        from: process.env.EMAIL_USER,
-        to: email,
-        subject: 'Your Verification code',
-        html: `
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch("https://api.brevo.com/v3/smtp/email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "api-key": process.env.BREVO_API_KEY,
+        },
+        body: JSON.stringify({
+          sender: { email: process.env.EMAIL_USER },
+          to: [{ email }],
+          subject: 'Your Verification code',
+          htmlContent: `
           <p>Your verification code is: <strong>${code}</strong></p>
           <p>Or click the link below to verify:</p>
           <a href="${magicLink}">Verify via this Link</a>
         `,
-        text: `Your verification code is : ${code} \n Or click the link below to verify:\n${magicLink}`,
-      },
-      (err, info) => {
-        if (err) {
-          console.error('error sending OTP:', err);
-          return reject(new Error('failed to send OTP'));
-        }
-        console.log(`OTP sent to ${email}`);
-        resolve(info);
+          textContent: `Your verification code is : ${code} \n Or click the link below to verify:\n${magicLink}`,
+        }),
+      });
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.error('error sending OTP:', errorData);
+        return reject(new Error('failed to send OTP'));
       }
-    );
+      const data = await response.json();
+      console.log(`OTP sent to ${email}`);
+      resolve(data);
+    } catch (error) {
+      console.error('error sending OTP:', error);
+      reject(new Error('failed to send OTP'));
+    }
   });
 };
+
